2e6697c6287c8eeda95149cbb46a6add89ecd7f3
unknown <imothoth@gmail.com>
2022-01-19 17:44:19 +0000

    React

---
+++ reviews/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Review from './Review';
+import {FaDev} from 'react-icons/fa';
+
+function App() {
+  return (
+    <>
+    <FaDev className="icon" />
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our Reviews</h2>
+            <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ tours/src/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = () => {
+
+    return (
+        <div className="loading">
+            <h1>LoAdInG.....lOaDiNg</h1>
+        </div>
+    )
+
+}
+
+export default Loading;
